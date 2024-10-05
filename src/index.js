687273c517b17ac731923c2fa1df2a375f986e55
edu-barrera <barreracarloseduardo03@gmail.com>
2024-10-04 21:27:09 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Inicio() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <div className="body_index">
+          <div className="container mt-12">
+            <div className="row align-items-center">
+              {/* Primera columna */}
+              <div className="col-sm-6 col-md-4 col-lg-4 text-center mb-4">
+                <h2 className="titulos">
+                  Platícanos tu idea y conviértela en realidad
+                </h2>
+                <img
+                  src="../recursos/Iconos/galletas_prin.png"
+                  alt="galletas festejo"
+                  className="img-fluid rounded"
+                />
+                <p className="pregunta">
+                  ¿Necesitas algo especial? ¡Nosotros te lo hacemos!
+                </p>
+                <a href="productos.html#cards-etiquetas">
+                  <button className="btn" id="botonGalletas">
+                    Descubre nuestras galletas
+                  </button>
+                </a>
+              </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+              {/* Segunda columna */}
+              <div className="col-sm-6 col-md-4 col-lg-4 text-center mb-4">
+                <img
+                  src="../recursos/Iconos/enam.jpg"
+                  alt="galleta_con_corazones"
+                  className="img-fluid rounded"
+                />
+              </div>
+
+              {/* Tercera columna */}
+              <div className="col-sm-6 col-md-4 col-lg-4 text-center mb-4">
+                <h2 className="titulos">
+                  ¿Te gustaría aprender a hacer tus propias galletas?
+                </h2>
+                <img
+                  src="../recursos/Iconos/cocinando.png"
+                  alt="Persona horneando"
+                  className="img-fluid rounded"
+                />
+                <p>Aprende con nuestros cursos</p>
+                <a href="productos.html">
+                  <button className="btn" id="botonCursos">
+                    ¡Empieza a hornear!
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default Inicio;
